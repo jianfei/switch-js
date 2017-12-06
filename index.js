b91b9affccc2afe1ac031e1df35389b78fe3f7f7
飞酱 <zjf111435@alibaba-inc.com>
2017-12-06 21:16:06 +0800

    feat: change way of export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export default realValue => {
+module.exports = realValue => {
     let chain;
     let caseHandler;
     let map;
@@ -63,4 +63,4 @@ export default realValue => {
     }
 
     return chain;
-}
+};
