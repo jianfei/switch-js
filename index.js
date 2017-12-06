30d4adfea503c4ac1510a23f7e5e30542be41684
飞酱 <zjf111435@alibaba-inc.com>
2017-12-06 21:09:07 +0800

    feat: change way of export

--- index.js
+++ index.js
@@ -1,66 +1,83 @@
-export default realValue => {
-    let chain;
-    let caseHandler;
-    let map;
-    let returnValue;
-    let isLastCaseMatched = false;
-    let hasMatch = false;
+(function () {
+    'use strict';
 
-    caseHandler = (isMatched, matchedCallback, notMatchedCallback) => {
-        isLastCaseMatched = false;
+    function switcher(realValue) {
+        let chain;
+        let caseHandler;
+        let map;
+        let returnValue;
+        let isLastCaseMatched = false;
+        let hasMatch = false;
 
-        if (!hasMatch && isMatched) {
-            isLastCaseMatched = true;
-            hasMatch = true;
-            matchedCallback && matchCallback(realValue, isMatched);
-        }
-        else {
-            notMatchedCallback && notMatchedCallback(realValue, isMatched);
+        caseHandler = (isMatched, matchedCallback, notMatchedCallback) => {
+            isLastCaseMatched = false;
+
+            if (!hasMatch && isMatched) {
+                isLastCaseMatched = true;
+                hasMatch = true;
+                matchedCallback && matchCallback(realValue, isMatched);
+            }
+            else {
+                notMatchedCallback && notMatchedCallback(realValue, isMatched);
+            }
+
+            return chain;
+        },
+
+        chain = {
+            case: (caseValue, ...args) => caseHandler(_.isEqual(caseValue, realValue), ...args),
+
+            cases: (caseValues, ...args) => caseHandler(_.includes(caseValues, realValue), ...args),
+
+            caseWhen: (matcher, ...args) => caseHandler(matcher(realValue), ...args),
+
+            default: (noMatchFoundCallback, matchFoundCallback) => {
+                const callback = hasMatch ? matchFoundCallback : noMatchFoundCallback;
+
+                callback && callback(realValue, hasMatch);
+                isLastCaseMatched = true;
+
+                return chain;
+            },
+
+            always: callback => {
+                callback(realValue, hasMatch);
+
+                return chain;
+            },
+
+            to: toValue => {
+                if (_.isUndefined(returnValue) && isLastCaseMatched) {
+                    returnValue = toValue;
+                }
+
+                return chain;
+            },
+
+            mapTo: mapper => {
+                if (_.isUndefined(returnValue) && isLastCaseMatched) {
+                    returnValue = _.isFunction(mapper) ? mapper(realValue) : _.get(realValue, mapper);
+                }
+
+                return chain;
+            },
+
+            value: defaultValue => returnValue || defaultValue,
         }
 
         return chain;
-    },
-
-    chain = {
-        case: (caseValue, ...args) => caseHandler(_.isEqual(caseValue, realValue), ...args),
-
-        cases: (caseValues, ...args) => caseHandler(_.includes(caseValues, realValue), ...args),
-
-        caseWhen: (matcher, ...args) => caseHandler(matcher(realValue), ...args),
-
-        default: (noMatchFoundCallback, matchFoundCallback) => {
-            const callback = hasMatch ? matchFoundCallback : noMatchFoundCallback;
-
-            callback && callback(realValue, hasMatch);
-            isLastCaseMatched = true;
-
-            return chain;
-        },
-
-        always: callback => {
-            callback(realValue, hasMatch);
-
-            return chain;
-        },
-
-        to: toValue => {
-            if (_.isUndefined(returnValue) && isLastCaseMatched) {
-                returnValue = toValue;
-            }
-
-            return chain;
-        },
-
-        mapTo: mapper => {
-            if (_.isUndefined(returnValue) && isLastCaseMatched) {
-                returnValue = _.isFunction(mapper) ? mapper(realValue) : _.get(realValue, mapper);
-            }
-
-            return chain;
-        },
-
-        value: defaultValue => returnValue || defaultValue,
     }
 
-    return chain;
-}
+    if (!_.isUndefined(module) && module.exports) {
+        module.exports = switcher;
+    }
+    else if (_.isFunction(define) && _.isObject(define.amd) && define.amd) {
+        define('switcher', [], function () {
+            return switcher;
+        });
+    }
+    else {
+        window.switcher = switcher;
+    }
+
+});
